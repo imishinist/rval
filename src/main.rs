b33f3fc498b2b923471a3f8d3d30e90097449933
Taisuke Miyazaki <imishinist@gmail.com>
2021-03-26 20:25:41 +0900

    add option

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use anyhow::Result;
 
+use clap::{clap_app, crate_authors, crate_description, crate_version};
 use rval::data::Spec;
 use rval::{data::Scenario, play::Player};
 
 fn main() -> Result<()> {
-    let spec = Spec::builder().status(200).build();
-    let scenario = Scenario::new("test".into(), "https://google.com".into(), 5, spec);
+    let matches = clap_app!(rval =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg name: --name [NAME] "name of scenario")
+        (@arg status: -s --status [STATUS] "http status code")
+        (@arg url: --url [URL] "request url")
+        (@arg num: -n --num [NUM] "request count")
+    )
+    .get_matches();
+
+    let name = matches.value_of("name").unwrap_or("ping");
+    let status = matches.value_of("status").unwrap_or("200").parse::<u16>()?;
+    let url = matches.value_of("url").unwrap();
+    let num = matches.value_of("num").unwrap_or("5").parse::<usize>()?;
+
+    let spec = Spec::builder().status(status).build();
+    let scenario = Scenario::new(name.into(), url.into(), num, spec);
     let player = Player::new(scenario);
     player.play()?;
 
