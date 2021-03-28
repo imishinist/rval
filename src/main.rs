f4b60d6c3276cd6f364f4193c914a89cb5518483
Taisuke Miyazaki <imishinist@gmail.com>
2021-03-28 20:04:38 +0900

    use value_t! macro

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use anyhow::{anyhow, Result};
-use clap::{clap_app, crate_authors, crate_description, crate_version};
+use clap::{clap_app, crate_authors, crate_description, crate_version, value_t};
 
 use rval::data::Spec;
 use rval::pace::{NonStop, Pacer, Rate};
@@ -13,8 +13,8 @@ fn main() -> Result<()> {
         (author: crate_authors!())
         (about: crate_description!())
         (@arg name: --name [NAME] "name of scenario")
-        (@arg status: -s --status [STATUS] "http status code")
         (@arg url: --url [URL] "request url")
+        (@arg status: -s --status [STATUS] "http status code")
         (@arg num: -n --num [NUM] "request count")
         (@arg worker: -w --worker [NUM] "workers count")
         (@arg freq: -f --freq [NUM] "request frequency")
@@ -23,11 +23,15 @@ fn main() -> Result<()> {
     .get_matches();
 
     let name = matches.value_of("name").unwrap_or("ping");
-    let status = matches.value_of("status").unwrap_or("200").parse::<u16>()?;
     let url = matches.value_of("url").unwrap();
-    let num = matches.value_of("num").unwrap_or("5").parse::<usize>()?;
-    let worker = matches.value_of("worker").unwrap_or("5").parse::<usize>()?;
-    let freq = matches.value_of("freq").unwrap_or("1").parse::<u128>()?;
+
+    // for spec
+    let status = value_t!(matches, "status", u16).unwrap_or(200);
+
+    // for run setting
+    let num = value_t!(matches, "num", usize).unwrap_or(5);
+    let worker = value_t!(matches, "worker", usize).unwrap_or(5);
+    let freq = value_t!(matches, "freq", u128).unwrap_or(1u128);
 
     let pacer = match matches.value_of("pacer").unwrap() {
         "rate" => Ok(Box::new(Rate::new(freq, Duration::from_secs(1))) as Box<dyn Pacer>),
